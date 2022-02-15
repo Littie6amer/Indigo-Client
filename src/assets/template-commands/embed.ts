11eaeee3cbf55beb8d628cd97f9e2f7d7e199a71
Littie6amer <toby5.tc@gmail.com>
2022-02-15 15:58:41 +0000

    Finishing the template commands

--- src/assets/template-commands/embed.ts
+++ src/assets/template-commands/embed.ts
@@ -7,13 +7,15 @@ const slashCommand = new SlashCommand({
 })
 
 slashCommand.setOptions([
-    new SubCommandOption({ name: "quote", description: "Create an embed quote of yourself.", execute: (interaction) => {
-        const embed = new MessageEmbed()
-            .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL()||undefined})
-            .setDescription(`${interaction.options.getString("text")}`)
-            .setColor("GREEN")
-        interaction.reply({ embeds: [embed] })
-    }}).setOptions([
+    new SubCommandOption({
+        name: "quote", description: "Create an embed quote of yourself.", execute: (interaction) => {
+            const embed = new MessageEmbed()
+                .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() || undefined })
+                .setDescription(`${interaction.options.getString("text")}`)
+                .setColor("GREEN")
+            interaction.reply({ embeds: [embed] })
+        }
+    }).setOptions([
         new ValueOption({ name: "text", type: "STRING", description: "Embed Description" }).setRequired(),
     ]),
 ])
--- src/assets/template-commands/stats.ts
+++ src/assets/template-commands/stats.ts
@@ -2,12 +2,9 @@ import { SlashCommand, SubCommandOption } from "../../lib/index.js"
 import { CommandInteraction } from "discord.js"
 
 const slashCommand = new SlashCommand({
-    name: "stats",
-    description: "General Bot Statitics",
-    execute: (interaction, toolbox) => {
-        interaction.reply({ content: `${toolbox}` })
-        console.log(interaction.client)
-    },
+    name: "ping",
+    description: "Get the bot's ping!",
+    execute: (interaction, toolbox) => interaction.reply({ content: `${interaction.client.ws.ping}` }),
 })
 
 export default [slashCommand]
