21704a794b6cee72e10d520c903004b665192d54
Littie6amer <toby5.tc@gmail.com>
2022-04-23 19:52:59 +0100

    update build

--- build/bases/SlashCommandBase.js
+++ build/bases/SlashCommandBase.js
@@ -11,7 +11,8 @@ class SlashCommandBase {
         this.isSubcommand = false;
     }
     run(optionNames, client, interaction) {
-        const subcommand = optionNames[0] ? this.subcommands.find(subcommand => subcommand.name == optionNames.shift()) : null;
+        const subcommand = optionNames[0] ? this.subcommands.find(subcommand => subcommand.name == optionNames[0]) : null;
+        optionNames.shift();
         if (!subcommand)
             this.execute(client, interaction);
         else
