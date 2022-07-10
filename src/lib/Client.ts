0f7fffc5d68c4642c689ac5ae06e474eb32cd77f
Littie6amer <toby5.tc@gmail.com>
2022-07-10 15:39:07 +0100

    changed a few client properties

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -9,16 +9,16 @@ import { SlashCommandManager } from "../modules/SlashCommandManager";
 export class Client extends DiscordJS.Client {
     rootPath: string = require?.main?.path || ""
     toolbox: Toolbox;
-    eventManager: ClientEventManager;
-    slashCommandManager: SlashCommandManager;
+    events: ClientEventManager;
+    commands: SlashCommandManager;
     constructor(options: ClientOptions) {
         super({ intents: options.intents, ws: { properties: { browser: options.mobileStatus ? "Discord iOS" : undefined } } });
         const eventFolders = options.eventFolders?.map(folder => this.rootPath + path.sep + folder) || []
         if (options?.defaultEvents !== false) eventFolders?.push(path.resolve(__dirname, "../default-events"))
-        this.eventManager = new ClientEventManager({ client: this, folders: eventFolders });
+        this.events = new ClientEventManager({ client: this, folders: eventFolders });
         this.toolbox = new Toolbox()
         const commandFolders = options.commandFolders?.map(folder => this.rootPath + path.sep + folder) || []
-        this.slashCommandManager = new SlashCommandManager({ client: this, folders: commandFolders })
-        if (options?.events) this.eventManager.registerEvents(options.events)
+        this.commands = new SlashCommandManager({ client: this, folders: commandFolders })
+        if (options?.events) this.events.registerEvents(options.events)
     }
 }
